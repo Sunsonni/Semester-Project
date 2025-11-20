6e99cbe406c4ceb53c2a1850b96772ededc4dd49
sunsonni <sonniemnguyen@gmail.com>
2025-11-19 19:30:24 -0600

    wip

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { authGuard } from './guards/auth.guard';
 import { loginGuard } from './guards/login.guard';
 import { RegisterPageComponent } from './register-page/register-page.component';
 import { ChatPageComponent } from './chat-page/chat-page.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 export const routes: Routes = [
+    { path: '', component: LoginPageComponent },
+    { path: 'register', component: RegisterPageComponent },
     { path: 'login', component: LoginPageComponent, canActivate: [loginGuard] },
     { path: 'home', component: HomePageComponent, canActivate: [authGuard] },
     { path: 'chat', component: ChatPageComponent, canActivate: [authGuard] },
-    { path: 'register', component: RegisterPageComponent },
-    // TODO: Create error page
-    { path: '**', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'error', component: ErrorPageComponent },
+    { path: '**', redirectTo: 'error' },
 ];
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,7 +1,8 @@
 import { NgIf } from '@angular/common';
 import { Component, effect } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { RouterLink } from '@angular/router';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-nav-bar',
@@ -13,10 +14,33 @@ import { RouterLink } from '@angular/router';
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
-  constructor (private auth: AuthService) {}
+  currentRoute: string = '';
+  constructor (
+    private auth: AuthService,
+    private router: Router
+  ) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      this.currentRoute = event.urlAfterRedirects;
+    });
+  }
 
   get isLoggedIn() : Boolean {
     return this.auth.isLoggedIn() || !!sessionStorage.getItem('token');
+  }
+
+  get buttonText() : string {
+    if (this.currentRoute.includes('login')) return 'Register';
+    if (this.currentRoute.includes('register')) return 'Login';
+    if (this.currentRoute === '/') return 'Register';
+    return 'Login';
+  }
+
+  get buttonLink() : string{
+    if (this.currentRoute.includes('login')) return '/register';
+    if (this.currentRoute.includes('register')) return '/login';
+    return '/login';
   }
 
   logout(){
